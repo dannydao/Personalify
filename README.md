628e3d764bf41d48232f438fff6deee396ee6e15
dannydao <dannyhoangdao@gmail.com>
2025-09-25 03:43:29 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Personalify
+Discover your MBTI personlity type based on your Spotify listening habits.
+Built with Django (backend), React (frontend) and the Spotify Web API
 
+---
+
+## Features
+- Spotify Login: authenticate with your Spotify account.
+- Top Tracks & Artists: fetches your listening history.
+- MBTI Engine: maps genre and audio features to MBTI traits.
+- AI Assist: uses AI to refine personality predictions.
+- Playlist & Suggestions: recommends music based on your type.
+
+## Tech Stack
+- Backend: Python, Django, Django REST framework
+- Frontend: React + Vite (to be added later)
+- Database: PostgresSQL (dev uses SQLite)
+- Deployment: Docker, Nginx, Gunicorn
---
+++ backend/config/settings.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
+
+SECRET_KEY = os.getenv("SECRET_KEY", "fallback-secret")
+
+DEBUG = os.getenv("DEBUG", "True").lower() == "true"
+
+# Allow local dev
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "core",
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "configs.url"
+
+TEMPLATES = [
+    {
+        "BACKENDS": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "core" / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.contect_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages"
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+# Database (simple sqlite for now)
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3"
+    }
+}
+
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
